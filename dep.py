616dcf6c6d9a6c5e6cc67eae7e7e05d5b948333a
LIAMSIIII <141633911+LIAMSIIII@users.noreply.github.com>
2024-03-13 12:05:13 +0000

    dep.py

--- dep.py
+++ dep.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 
 # loading the saved model
-loaded_model = pickle.load(open('random_forest_model.pkl.sav','rb'))
+loaded_model = pickle.load(open('random_forest_model.pkl','rb'))
 
 
 # creating a function for Prediction
